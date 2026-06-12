ab560d2b94df4e2f750c9ef9fea14fd6017aab8a
steve-shih <steve-shih@users.noreply.github.com>
2026-06-12 23:36:39 +0800

    Add environment configs and GitHub Actions

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
+
+load_dotenv()
 
 # Fetch connection URI from environment, default to local MongoDB
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
